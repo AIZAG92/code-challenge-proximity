a8965f9bdfb309dee7d6bf1c3dee1118e5b67131
Ana Izaguirre <seshomaru1010@gmail.com>
2020-01-16 01:26:17 -0600

    Document route csv

--- routes/csv.js
+++ routes/csv.js
@@ -4,6 +4,16 @@ const multer = require('multer');
 const upload = multer({ dest: 'tmp/csv/' })
 const { parseCsv } = require('../controllers/api/csv');
 
+/**
+ * Creates a new client account.
+ *
+ * @name Create a txt file from a csv 
+ * @route {POST} api/v1/csv/:providerName
+ * @routeparam  {string} providerName - the providername that sent the file.
+ * @bodyparam   {file}  file - The csv file to be sent. 
+ *
+ * @return {Object} PLACEHOLDER
+ */
 router.post('/:providerName', upload.single('file'), parseCsv);
 
 module.exports = router; 
